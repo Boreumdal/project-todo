4f54a8662f8e5960e4dbc1f06821d575e39404cb
Melvin Arellano <lmelvin.arellano@gmail.com>
2023-03-23 18:34:13 +0800

    done task items

--- src/js/index-js.js
+++ src/js/index-js.js
@@ -8,6 +8,11 @@ if (!username){
 
 let login_container = document.querySelector('.login-container') // login container
 let login_input = document.querySelector('#login-input')
+
+const upperCaseFirst = str => {
+    return `${str[0].toUpperCase()}${str.slice(1)}`
+}
+
 login.addEventListener('submit', e => {
     e.preventDefault()
 
@@ -16,7 +21,10 @@ login.addEventListener('submit', e => {
     if (username){
         login_container.style.display = 'none'
         todo.style.display = '' 
+        
+        let nameSpan = document.querySelector('span.name')
+
+        nameSpan.textContent = upperCaseFirst(username)
     }
 })
-
 
